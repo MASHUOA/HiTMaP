6536a9e248036b18b0c3f12011ad77a9e418cfaa
guoguodigit <guoguodigital@msn.com>
2019-08-02 12:07:38 +1200

    fix

--- R/Canidate_processing.R
+++ R/Canidate_processing.R
@@ -216,15 +216,18 @@ Protein_feature_list_fun1<-function(workdir=getwd(),
   for (i in 1:length(adducts)){
     adductmass<-as.numeric(as.character(adductslist[adductslist$Name==adducts[i],"Mass"]))
     multiplier<-as.numeric(as.character(adductslist[adductslist$Name==adducts[i],"Multi"]))
+    
     adductsformula<-c(as.character(adductslist[adductslist$Name==adducts[i],"Formula_add"]),
                       as.character(adductslist[adductslist$Name==adducts[i],"Formula_ded"]))
+    
     adductsformula=paste0(ifelse(adductsformula=="FALSE","",adductsformula),collapse = "")
     
     tempdf$mz<-as.numeric(parentIonMass(tempdf$Peptide)-1.007825+adductmass)
     tempdf$adduct<-adducts[i]
     tempdf$isdecoy<-rep(0,nrow(tempdf))
-    convert_peptide_adduct(tempdf$Peptide[2],adductsname = adducts[i],multiplier = c(multiplier,1),adductslist = adductslist)
-    tempdf$formula<-bplapply(tempdf$Peptide,convert_peptide_adduct,adductsformula = adducts[i],multiplier = c(multiplier,1),BPPARAM = BPPARAM)
+    #convert_peptide_adduct(tempdf$Peptide[2],adductsname = adducts[i],multiplier = c(multiplier,1),adductslist = adductslist)
+    message(paste("Building candidates list for adduct",adducts[i])) 
+    tempdf$formula<-bplapply(tempdf$Peptide,convert_peptide_adduct,adductsname = adducts[i],adductslist=adductslist,multiplier = c(multiplier,1),BPPARAM = BPPARAM)
     Protein_Summary<-rbind.data.frame(Protein_Summary,tempdf)
   }
   tempdf<-tempdf1
@@ -244,8 +247,8 @@ vendiagram<-function(){
   library(tcltk)
   folder1<-tkchooseDirectory()
   folder2<-tkchooseDirectory()
-  Protein_feature_summary_uniport <- read.csv(paste0(folder1,"/Summary folder/Protein_feature_summary.csv"))
-  Protein_feature_summary_marine <- read.csv(paste0(folder2,"/Summary folder/Protein_feature_summary.csv"))
+  Protein_feature_summary_uniport <- read.csv(paste0(as.character(paste0(folder1,collapse = " ")),"/Summary folder/Protein_feature_summary.csv"))
+  Protein_feature_summary_marine <- read.csv(paste0(as.character(paste0(folder2,collapse = " ")),"/Summary folder/Protein_feature_summary.csv"))
   #Protein_feature_summary_nonmarine <- read.csv("D:/Tumour test/test9 new matrisome/Summary folder/Protein_feature_summary.csv")
   
   #Protein_feature_summary_uniport <- read.csv("D:/Tumour test/test12 new non matrisome gnil/Summary folder/Protein_feature_summary.csv")
@@ -284,9 +287,10 @@ vendiagram<-function(){
   
   uniport_prot_a=str_extract(uniport_prot,"\\|.{2,}\\|")
   matrisome_prot_a=str_extract(matrisome_prot,"\\|.{2,}\\|")
-  result[["prot0.05"]][1]<-length(uniport_prot)
   result[["prot0.05"]][2]<-length(matrisome_prot)
+  result[["prot0.05"]][1]<-length(uniport_prot)
   result[["prot0.05"]][3]<-sum(uniport_prot %in% matrisome_prot)
+  result<-data.frame(result,stringsAsFactors = F)
   return(result)
   #protein_unique=unique(Protein_feature_list$mz)
 }
@@ -360,27 +364,123 @@ fastafile_utils<-function(){
 convert_peptide_adduct<-function(peptide,adductsname,multiplier=c(1,1),adductslist=Build_adduct_list()){
   library(rcdk)
   library(OrgMassSpecR)
+  
+  get_atoms<-function(Symbol){
+    #form = "C5H11BrO" 
+    if (Symbol!=""){
+      ups = c(gregexpr("[[:upper:]]", Symbol)[[1]], nchar(Symbol) + 1) 
+      seperated = sapply(1:(length(ups)-1), function(x) substr(Symbol, ups[x], ups[x+1] - 1)) 
+      elements =  gsub("[[:digit:]]", "", seperated) 
+      nums = gsub("[[:alpha:]]", "", seperated) 
+      ans = data.frame(element = as.character(elements), num = as.numeric(ifelse(nums == "", 1, nums)), stringsAsFactors = FALSE)
+      list<-as.list(ans$num)
+      names(list)=ans$element
+      return(list)
+    }else{return(NULL)}
+  }
+  
+  
   adductsformula_add= as.character(adductslist[adductslist$Name==adductsname,"Formula_add"])
   adductsformula_ded= as.character(adductslist[adductslist$Name==adductsname,"Formula_ded"])
   
-  null_if_false<-function(x){if (x==FALSE){""} else{get.formula(x)}}
+  null_if_false<-function(x){if (x==FALSE){""} else{get.formula(x)@string}}
   
   adductsformula_add<-null_if_false(adductsformula_add)
   adductsformula_ded<-null_if_false(adductsformula_ded)
   
+  adductsformula_add<-get_atoms(adductsformula_add)
+  adductsformula_ded<-get_atoms(adductsformula_ded)
+  
+  adductsformula<-merge_atoms(adductsformula_add,adductsformula_ded,check_merge = F,mode = "ded")
+  
   formula<-ConvertPeptide(peptide,IAA = F)
-  if (multiplier[1]!=1){
-    lapply(names(formula),FUN = function(x,formula,multiplier){
-      formula[[x]]=formula[[x]]*multiplier[1]
-    },formula,multiplier)
-   formula[[adductsformula]]= formula[[adductsformula]]+multiplier[2] 
-  }else{
-   formula[[adductsformula]]= formula[[adductsformula]]+multiplier[2]
-  }
-  for (name in names(formula)){
-    if(formula[[name]]==0){ formula[[name]]=NULL}
+  
+  formula_with_adducts<-merge_atoms(formula,adductsformula,check_merge = T,mode = "add", multiplier = multiplier)
+  
+  for (name in names(formula_with_adducts)){
+    if(formula_with_adducts[[name]]==0){ formula_with_adducts[[name]]=NULL}
   }
 
-  paste0(names(formula),formula,collapse = "")
+  return(paste0(names(formula),formula,collapse = ""))
+  
+}
+
+#' get_atoms
+#'
+#' This is a function that prepare the candiate list for maldi imaging data qualitative or quantitative analysis.
+#' this function will read the candidate list file and generate mz for the adducts list defined in \code{"adducts"}. 
+#' @param database the file name of candidate list
+#' @param workdir the folder that contains candidate list
+#' @param adducts  the adducts list to be used for generating the PMF search candidates
+#' @param cal.mz If set with \code{"TRUE"}, the function will recalculate the mz value according to the column named "Formula" in the \code{database} and the specified adducts.
+#' @param mzlist_bypass  Set \code{"TRUE"} if you want to bypass the mzlist generating process, the function will keep the mz and adduct as it is for the furture analysis. Be sure that the table contains "mz", "adduct" and "moleculeNames" as they are essential for later steps.
+#' @param BPPARAM parallel processing parameter for BiocParallel
+#' @return a table of candiate list
+#'
+#' @examples
+#' Meta_feature_list_fun(database="lipid candidates.csv",adducts=c("M-H","M+Cl"))
+#'
+#' @export
+#' 
+get_atoms<-function(Symbol){
+  #form = "C5H11BrO" 
+  if (Symbol!=""){
+      ups = c(gregexpr("[[:upper:]]", Symbol)[[1]], nchar(Symbol) + 1) 
+  seperated = sapply(1:(length(ups)-1), function(x) substr(Symbol, ups[x], ups[x+1] - 1)) 
+  elements =  gsub("[[:digit:]]", "", seperated) 
+  nums = gsub("[[:alpha:]]", "", seperated) 
+  ans = data.frame(element = as.character(elements), num = as.numeric(ifelse(nums == "", 1, nums)), stringsAsFactors = FALSE)
+  list<-as.list(ans$num)
+  names(list)=ans$element
+  return(list)
+  }else{return(NULL)}
+}
+
+
+#' merge_atoms
+#'
+#' This is a function that prepare the candiate list for maldi imaging data qualitative or quantitative analysis.
+#' this function will read the candidate list file and generate mz for the adducts list defined in \code{"adducts"}. 
+#' @param database the file name of candidate list
+#' @param workdir the folder that contains candidate list
+#' @param adducts  the adducts list to be used for generating the PMF search candidates
+#' @param cal.mz If set with \code{"TRUE"}, the function will recalculate the mz value according to the column named "Formula" in the \code{database} and the specified adducts.
+#' @param mzlist_bypass  Set \code{"TRUE"} if you want to bypass the mzlist generating process, the function will keep the mz and adduct as it is for the furture analysis. Be sure that the table contains "mz", "adduct" and "moleculeNames" as they are essential for later steps.
+#' @param BPPARAM parallel processing parameter for BiocParallel
+#' @return a table of candiate list
+#'
+#' @examples
+#' Meta_feature_list_fun(database="lipid candidates.csv",adducts=c("M-H","M+Cl"))
+#'
+#' @export
+merge_atoms<-function(atoms,addelements,check_merge=T,mode=c("add","ded"),multiplier=c(1,1)){
+  
+  atomsorg=atoms
+  
+  if (missing(mode)) mode="add"
+  
+  if (mode=="ded"){
+    for (x in names(addelements)){
+      addelements[[x]]=-addelements[[x]]
+    }
+  }
+  
+  for (x in names(addelements)){
+    if (is.null(atoms[[x]])){
+      atoms[[x]]=addelements[[x]]
+    }else {
+      atoms[[x]]=(atoms[[x]]*multiplier[1]) + (addelements[[x]]*multiplier[2])
+    } 
+  }
+  
+  if (check_merge==F){
+    for (x in names(atoms)){
+      if (atoms[[x]]<0){
+      stop("add atoms failed due to incorrect number of",x)
+      } 
+    }
+    
+  }
+  return(as.list(atoms))
   
 }
