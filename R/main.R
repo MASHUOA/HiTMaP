505f196c1203bf9376259223b33ece1c8b018260
guoguodigit <guoguodigital@msn.com>
2019-09-20 14:43:06 +1200

    upd

--- R/main.R
+++ R/main.R
@@ -50,6 +50,7 @@
 #' @import zoo
 #' @import FTICRMS
 #' @import KEGGREST
+#' @import OrgMassSpecR
 #' @importFrom stats na.omit runif
 #' @importFrom utils download.file modifyList packageVersion read.table tail
 #' @import S4Vectors
